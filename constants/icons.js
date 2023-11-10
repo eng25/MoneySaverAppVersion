6783d19db4a9faa13252676c540f73aea789363c
erica <ely4@uic.edu>
2023-11-10 11:29:37 -0600

    subs tab:added logo, form exit, extra features

--- constants/icons.js
+++ constants/icons.js
@@ -2,6 +2,7 @@ const icons = {
     home: require('../../app/assets/icons/menu.png'),
     expenses: require('../../app/assets/icons/expenses.png'),
     subscriptions: require('../../app/assets/icons/subscriptions.png'),
+    hbo: require('../../app/assets/icons/hbo.png'),
   };
   
   export default icons;
